1ffcd37cc8118ca0a08d1393a5f925d78c999544
Austin Sun <austinsunc@yahoo.com>
2022-01-29 15:56:41 -0800

    transition properly working

--- Levels/SceneManager.gd
+++ Levels/SceneManager.gd
@@ -2,20 +2,29 @@ extends Node
 
 const World2D = preload("res://Levels/2DLevel1.tscn")
 const World3D = preload("res://Levels/3DLevel1.tscn")
-
+const Transition = preload("res://Levels/Transitions/Fade.tscn")
+var temp
 var is3D = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("Transition"):
-		$FadeTransition.transition()
+		temp = Transition.instance()
+		add_child(temp)
+		temp.connect("transitioned", self, "_on_FadeTransition_transitioned")
+		temp.transition()
 
 func _on_FadeTransition_transitioned():
+	print("hello")
 	$CurrentScene.get_child(0).queue_free()
+	print("test", is3D)
 	if(is3D):
 		$CurrentScene.add_child(World2D.instance())
 		is3D = false
 	else:
 		$CurrentScene.add_child(World3D.instance())
-		is3D = true 
+		is3D = true
+	print($CurrentScene.get_children())
+	temp.disconnect("transitioned", self, "_on_FadeTransition_transitioned")
+	temp.queue_free()
 	print("Swapped Scene")
