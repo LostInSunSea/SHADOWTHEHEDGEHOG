99df08be09a318cb94449cc72bf698c22bcacbdb
Michael LaPlante <theloneplant@gmail.com>
2022-01-29 18:45:17 -0800

    fixing levers

--- Environment/3DProps/Interactive/3DLever.gd
+++ Environment/3DProps/Interactive/3DLever.gd
@@ -13,6 +13,7 @@ onready var handle = $Handle
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Globals.LeverState[self.name] = false
 	pass # Replace with function body.
 
 func interact():
@@ -20,7 +21,7 @@ func interact():
 	Globals.toggleLeverState(self)
 	
 	var deg_to_radians = (lever_angle * 2) * PI / 180
-	if is_active:
+	if Globals.LeverState[self.name]:
 		handle.rotate_object_local(Vector3(1, 0, 0), deg_to_radians)
 	else:
 		handle.rotate_object_local(Vector3(1, 0, 0), -deg_to_radians)
